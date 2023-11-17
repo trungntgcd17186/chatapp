e30cda77b70ba2b2104e2da6821c7588150735b3
trungnguyen1311 <106722143+trungnguyen1311@users.noreply.github.com>
2023-11-17 20:33:39 +0700

    props destructuring

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -6,7 +6,7 @@ import Components from "unplugin-vue-components/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({ script: { propsDestructure: true } }),
     Components({
       resolvers: [
         AntDesignVueResolver({
