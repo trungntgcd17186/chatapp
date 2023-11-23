2b3517df3d15d89b382afc121fd6ce370cc39d41
trungnguyen1311 <106722143+trungnguyen1311@users.noreply.github.com>
2023-11-23 15:21:14 +0700

    Remove .env file from tracking

--- chat-app-backend/src/app.module.ts
+++ chat-app-backend/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,22 +8,25 @@ import { ChatModule } from './chat/chat.module';
 import { Conversation } from './entities/conversation.entity';
 import { Users } from './entities/users.entity';
 import { Message } from './entities/message.entity';
-import { AppGateway } from './websockets/chat.gateway';
 
 @Module({
   imports: [
     ChatModule,
     AuthModule,
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'Kun123456@!',
-      database: 'chat_app',
-      entities: [Users, Conversation, Message],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: configService.get<string>('DB_PASSWORD'),
+        database: 'chat_app',
+        entities: [Users, Conversation, Message],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
