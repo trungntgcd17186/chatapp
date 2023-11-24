c2dd316d48b30b04412500a3de1a8a19fc89da09
trungnguyen1311 <106722143+trungnguyen1311@users.noreply.github.com>
2023-11-24 17:38:29 +0700

    add domain

--- vite-project/src/api/index.ts
+++ vite-project/src/api/index.ts
@@ -3,7 +3,7 @@ import { useCookies } from "vue3-cookies";
 const { cookies } = useCookies();
 
 export const get = (url: string, withToken = false) => {
-  return axios.get(`https://3.106.2.251:9090${url}`, {
+  return axios.get(`http://nguyenthanhtrung.click:9090${url}`, {
     headers: {
       "Content-Type": "application/json",
       Authorization: withToken ? `Bearer ${cookies.get("access_token")}` : "",
@@ -12,7 +12,7 @@ export const get = (url: string, withToken = false) => {
 };
 
 export const post = (url: string, values: {}) => {
-  return axios.post(`https://3.106.2.251:9090${url}`, values, {
+  return axios.post(`http://nguyenthanhtrung.click:9090${url}`, values, {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${cookies.get("access_token")}`,
