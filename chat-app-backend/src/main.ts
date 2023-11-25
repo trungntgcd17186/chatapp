ea87646a02bc8472b548f2db2fcd2e7a7567e1f0
trungnguyen1311 <106722143+trungnguyen1311@users.noreply.github.com>
2023-11-25 15:07:53 +0700

    save

--- chat-app-backend/src/main.ts
+++ chat-app-backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { ServerOptions } from 'socket.io';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 export class SocketAdapter extends IoAdapter {
   createIOServer(
@@ -16,8 +17,13 @@ export class SocketAdapter extends IoAdapter {
   }
 }
 
+const httpsOptions = {
+  key: fs.readFileSync('./secrets/cert.key'),
+  cert: fs.readFileSync('./secrets/cert.crt'),
+};
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, { cors: true, httpsOptions });
   app.useWebSocketAdapter(new SocketAdapter(app));
 
   await app.listen(9090);
