129ec4855744755f1d649c61a476090ccf89dc68
trungnguyen1311 <106722143+trungnguyen1311@users.noreply.github.com>
2023-11-26 00:12:09 +0700

    save

--- chat-app-backend/src/app.controller.ts
+++ chat-app-backend/src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/api')
+  getHelloApi(): string {
+    return this.appService.getHelloApi();
+  }
 }
