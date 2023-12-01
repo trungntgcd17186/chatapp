a2f55c82c65a7e0a95021ce2fda6afefd76ddfb9
trungnguyen1311 <106722143+trungnguyen1311@users.noreply.github.com>
2023-12-01 22:29:03 +0700

    save

--- vite-project/src/hooks/useMedia.ts
+++ vite-project/src/hooks/useMedia.ts
@@ -1,9 +1,9 @@
 import { ref, watchEffect } from 'vue';
 
-export default () => {
-  const isMobile = ref(window.matchMedia('(max-width: 767px)').matches);
+export default (maxWidth?: 767) => {
+  const isMobile = ref(window.matchMedia(`(max-width: ${maxWidth}px)`).matches);
   watchEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width: 767px)');
+    const mediaQuery = window.matchMedia(`(max-width: ${maxWidth}px)`);
     isMobile.value = mediaQuery.matches;
 
     const updateScreenSize = () => {
